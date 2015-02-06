117676baf5fa00819e5a0ef2ebfc725a6f4acc0f
= <=>
2015-02-06 02:16:46 +0000

    GREEN - REFACTOR IN TEST: Remove redundant code, slight improvements

--- FizzBuzzKata.Test/KataTest.cs
+++ FizzBuzzKata.Test/KataTest.cs
@@ -6,18 +6,17 @@ namespace FizzBuzzKata.Test
     [TestClass]
     public class KataTest
     {
-        [TestMethod]
+        FizzBuzz _fizzBuzz;
+        [TestInitialize]
         public void CanInitializeFizzBuzz()
         {
-            var fizzBuzz = new FizzBuzz();
+            _fizzBuzz = new FizzBuzz();
         }
 
         [TestMethod]
         public void CanPassOne()
         {
-            var fizzBuzz = new FizzBuzz();
-            // CHANGESET NOTE: I messed it up and sent 0 at the beggining should've been one!
-            string actual = fizzBuzz.Run(1);
+            string actual = _fizzBuzz.Run(1);
             string expected = "1";
 
             Assert.AreEqual(actual, expected);
@@ -26,8 +25,7 @@ namespace FizzBuzzKata.Test
         [TestMethod]
         public void ReturnsFizzForThree()
         {
-            var fizzBuzz = new FizzBuzz();
-            string actual = fizzBuzz.Run(3);
+            string actual = _fizzBuzz.Run(3);
             string expected = "Fizz";
 
             Assert.AreEqual(actual, expected);
