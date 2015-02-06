9963bbb5487d9e0f929de390889c9548ebf36772
= <=>
2015-02-06 02:01:47 +0000

    GREEN: Inital project with empty test file and no implementation

---
+++ FizzBuzzKata.Test/KataTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FizzBuzzKata.Test
+{
+    [TestClass]
+    public class KataTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
