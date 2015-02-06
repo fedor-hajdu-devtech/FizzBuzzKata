ad65173788c17522fbe1bfe6381a6501e2dc1627
= <=>
2015-02-06 02:22:39 +0000

    GREEN: Add logic to return FizzBuzz for numbers dividable by both 3 and 5

--- FizzBuzzKata/FizzBuzz.cs
+++ FizzBuzzKata/FizzBuzz.cs
@@ -4,6 +4,7 @@
     {
         public string Run(int input)
         {
+            if (input % 3 == 0 && input % 5 == 0) return "FizzBuzz";
             if (input % 3 == 0) return "Fizz";
             if (input % 5 == 0) return "Buzz";
             return input.ToString();
