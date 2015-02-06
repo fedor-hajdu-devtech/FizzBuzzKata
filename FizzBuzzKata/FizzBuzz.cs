695a59c31c19d64628feec4950716ccfaccb4aa0
= <=>
2015-02-06 02:19:18 +0000

    GREEN: Add logic to return Buzz for dividable by 5 numbers.

--- FizzBuzzKata/FizzBuzz.cs
+++ FizzBuzzKata/FizzBuzz.cs
@@ -1,16 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace FizzBuzzKata
+﻿namespace FizzBuzzKata
 {
     public class FizzBuzz
     {
         public string Run(int input)
         {
             if (input % 3 == 0) return "Fizz";
+            if (input % 5 == 0) return "Buzz";
             return input.ToString();
         }
     }
